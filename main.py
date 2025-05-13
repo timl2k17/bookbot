1eec131a711117d069135e6f6d5a9b1a471f2df8
Tim LaDuca <tjl@me.com>
2025-05-13 16:18:02 -0700

    final project

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 #  read the contents of a file named 'frankenstein.txt' located in a 'books' directory and print it to the console.
-from stats import word_count
+from stats import *
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    BOOK = sys.argv[1]
+
 
 def get_book_text(path):
     return open(path).read()
 
 
 def main():
-    print(f"{word_count(get_book_text('books/frankenstein.txt'))} words found in the document")
+    #print(f"{word_count(get_book_text(BOOK))} words found in the document")
+    #print(character_stats(get_book_text(BOOK)))
+    #print(sorted_character_stats(character_stats(get_book_text(BOOK))))
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {BOOK}")
+    print("---------- Word Count -----------")
+    print(f"Found {word_count(get_book_text(BOOK))} total words")
+    print("------- Character Count ---------")
+    for character in sorted_character_stats(character_stats(get_book_text(BOOK))):
+        if character['character'].isalpha():
+            print(f"{character['character']}: {character['count']}")
+    print("============ END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,25 @@
 def word_count(text):
     return len(text.split())
+
+def character_stats(text):
+    char_dict = {}
+    
+    for char in text:
+        if char.lower() in char_dict:
+            char_dict[char.lower()] += 1
+        else:
+            char_dict[char.lower()] = 1
+    return char_dict
+
+def sort_on(dict):
+    return dict["count"]
+
+def sorted_character_stats(character_dict):
+    character_list =[]
+    for char in character_dict:
+        character_list.append({
+            'character': char,
+            'count': character_dict[char]
+        })
+    character_list.sort(reverse=True, key=sort_on)
+    return character_list
