2869efdd9ef2419d2c731dd4a5b1b67ac3e9ace6
Tim LaDuca <tjl@me.com>
2025-05-13 15:30:07 -0700

    refactor

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 #  read the contents of a file named 'frankenstein.txt' located in a 'books' directory and print it to the console.
+from stats import word_count
 
 def get_book_text(path):
-    with open(path) as f:
-        contents = f.read()
-    return contents
+    return open(path).read()
 
-def word_count(text):
-    words = text.split()
-    return len(words)
 
 def main():
     print(f"{word_count(get_book_text('books/frankenstein.txt'))} words found in the document")
